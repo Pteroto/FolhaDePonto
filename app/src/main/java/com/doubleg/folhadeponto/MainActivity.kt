b39d0e9e37061cb84e13533a61e9a5a93f42ede6
Gustavo Monteiro <gustavosmonteiro@outlook.com>
2021-05-20 23:08:37 -0300

    Add bottom navigation

--- app/src/main/java/com/doubleg/folhadeponto/MainActivity.kt
+++ app/src/main/java/com/doubleg/folhadeponto/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.doubleg.folhadeponto
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.doubleg.folhadeponto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_home -> {
+                    binding.textViewTitle.text = getString(R.string.title_home)
+                    true
+                }
+                R.id.navigation_dashboard -> {
+                    binding.textViewTitle.text = getString(R.string.title_dashboard)
+                    true
+                }
+                R.id.navigation_notifications -> {
+                    binding.textViewTitle.text = getString(R.string.title_notifications)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.selectedItemId = R.id.navigation_home
     }
 }
