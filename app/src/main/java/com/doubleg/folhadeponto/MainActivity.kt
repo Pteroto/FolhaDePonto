e32d3f0f2e49612c665a4f1e9e14c4f1887faa4f
Gustavo Monteiro <gustavosmonteiro@outlook.com>
2021-05-20 21:13:15 -0300

    Initial commit

---
+++ app/src/main/java/com/doubleg/folhadeponto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.doubleg.folhadeponto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
